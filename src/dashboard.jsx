5a964bef6aeb6c68895222c2a6d86f2e2b3bd081
Athithyan24 <athithyan.802319@sxcce.edu.in>
2026-02-23 21:32:29 +0530

    teashopediting

---
+++ src/dashboard.jsx
@@ -0,0 +1,12 @@
+export default function Dashboard() {
+  return (
+    <>
+      <div>
+        <section className="section_one">
+          <div></div>
+        </section>
+        <section className="section_two"></section>
+      </div>
+    </>
+  );
+}
